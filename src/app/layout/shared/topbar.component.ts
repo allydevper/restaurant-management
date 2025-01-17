f051168d5a939c13c7428dfc4a74f7fb3d48f09b
allydevper <alamaswilmer@gmail.com>
2025-01-16 23:25:04 -0500

    close login

--- src/app/layout/shared/topbar.component.ts
+++ src/app/layout/shared/topbar.component.ts
@@ -1,11 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { AvatarModule } from 'primeng/avatar';
+import { InputTextModule } from 'primeng/inputtext';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MenubarModule, AvatarModule, InputTextModule, ButtonModule],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.scss'
 })
 export class TopbarComponent {
+  items: MenuItem[] = [];
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Bienvenido ' + this.authService.currentUserValue?.username,
+      }
+    ];
+  }
+
+  logout() {
+    this.authService.clearUser();
+    this.router.navigate(['/login']);
+  }
 }
