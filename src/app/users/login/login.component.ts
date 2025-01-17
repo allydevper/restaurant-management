241945074807b0baec9eefc96bf4d4a1c7a101fb
allydevper <alamaswilmer@gmail.com>
2025-01-16 20:53:11 -0500

    update form login

--- src/app/users/login/login.component.ts
+++ src/app/users/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
@@ -8,17 +8,23 @@ import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule, InputTextModule, PasswordModule, ButtonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, InputTextModule, PasswordModule, ButtonModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  username: string = '';
-  password: string = '';
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
 
   onSubmit() {
-    // Lógica para manejar el envío del formulario
-    console.log('Username:', this.username);
-    console.log('Password:', this.password);
+    console.log('Username:', this.loginForm.get('username')?.value);
+    console.log('Password:', this.loginForm.get('password')?.value);
   }
 }
